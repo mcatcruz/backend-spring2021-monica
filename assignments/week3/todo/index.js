0e209233d045647c9586a2466449834037e249ee
Monica C <mcatcruz@gmail.com>
2021-04-14 15:27:20 -0700

    Finished To-Do App  CSS, started JS.

--- assignments/week3/todo/index.js
+++ assignments/week3/todo/index.js
@@ -1,1 +1,39 @@
 const express = require('express');
+const fs = require('fs');
+const task = require('./Task.js');
+
+const app = express();
+
+const http = require('http').Server(app);
+
+const port = 3000;
+
+const char = 'utf-8';
+
+http.listen(port);
+
+console.log('Express server is now running on port ' + port);
+
+let tasks;
+let taskFileName = tasks.json;
+
+// Prepare JSON tasks file
+if (fs.existsSync(taskFileName)) {
+    let fileContents = fs.readFileSync(taskFileName, char);
+    tasks = JSON.parse(fileContents);
+} else {
+    tasks = {
+        incomplete: [],
+        completed: [],
+        deleted: []
+    }
+
+    fs.writeFileSync(taskFileName, JSON.stringify(tasks), char);
+}
+
+// Body Parser
+app.use(express.json({strict: false}));
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use('/', express.static('public_html/'));
--- assignments/week3/todo/public_html/scripts/main.js
+++ assignments/week3/todo/public_html/scripts/main.js
@@ -1,3 +1,20 @@
-$(function ()) {
-    console.log('hello');
-}
+$(function () {
+    $('#new-task div button:last-child').click(function () {
+        let parts = $("new-task div input[type-date]").val().split('-');
+        console.log(parts);
+        
+        let timeObject = new Date();
+        let testObject = new Date($('#new-task div input[type=date').val());
+        let anotherObject = new Date(parts[0], parts[1] -1, parts[2]);
+        timeObject.setDate(parts[2]);
+        timeObject.setFullYear(parts[0]);
+        timeObject.setMonth(parseInt(parts[1])-1);
+
+
+        console.log('actual ' + timeObject.toString());
+        console.log('test ' + testObject.toString());
+        console.log('actual2' + anotherObject.toString());
+    });
+
+});
+
